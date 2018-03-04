2b60540d77442cb04283706c770bd0adacc0b46e
Will Hay <willhay@Wills-MacBook-Pro.local>
2018-03-04 15:32:28 -0500

    fix provider bug

--- client/app/start/start.component.ts
+++ client/app/start/start.component.ts
@@ -7,8 +7,7 @@ import { ChangeDetectorRef } from '@angular/core';
 @Component({
   selector: 'start',
   templateUrl: './start.component.html',
-  styleUrls: ['./start.component.css'],
-  providers: [ AuthService ]
+  styleUrls: ['./start.component.css']
 })
 export class StartComponent implements OnInit {
 
