bfa1a3c651871e6f858bee24caf1b7264b794c06
Will Hay Jr <willhayjr@gmail.com>
2018-03-11 15:11:26 -0400

    finishing up bets

--- client/app/bet/bet.component.ts
+++ client/app/bet/bet.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { UserService } from '../services/user.service';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../services/auth.service';
 
 @Component({
   selector: 'app-bet',
@@ -7,16 +9,38 @@ import { UserService } from '../services/user.service';
   styleUrls: ['./bet.component.css']
 })
 export class BetComponent implements OnInit {
-  games:any;
+  games: any;
   isLoading = true;
   homePercentage: any;
   awayPercentage: any;
-  constructor(public userService: UserService) { }
+  betForm: any;
+
+  homeTeam: any = true;
+  awayTeam: any = false;
+
+  user: any;
+
+  amount = new FormControl('', [
+    Validators.required
+  ]);
+
+
+  constructor(public auth: AuthService, public userService: UserService, public formBuilder: FormBuilder) { }
 
   ngOnInit() {
     this.getGames();
+    this.getUser();
+    this.betForm = this.formBuilder.group({
+      amount: this.amount
+    });
   }
 
+  getUser() {
+    this.userService.getUser(this.auth.currentUser).subscribe(
+      data => this.user = data,
+      error => console.log(error)
+    );
+  }
 
   getGames() {
     this.userService.getGames().subscribe(
@@ -36,7 +60,7 @@ export class BetComponent implements OnInit {
   getTime(date) {
     var time = "";
     var datez = new Date(date);
-    datez.setHours(datez.getHours()+4);
+    datez.setHours(datez.getHours() + 4);
     time += datez.getHours();
     time += ":";
     time += datez.getMinutes();
@@ -44,7 +68,31 @@ export class BetComponent implements OnInit {
   }
 
   makeBet() {
-    
+    var homeAmount = 0;
+    var awayAmount = 0;
+    if (this.homeTeam) {
+      homeAmount = this.betForm.amount;
+    } else {
+      awayAmount = this.betForm.amount
+    }
+    var betObject = {
+      gameId: this.games[0].gameId,
+      userId: this.user.userId,
+      homeAmount: homeAmount,
+      awayAmount: awayAmount,
+      date: Date.now()
+    }
+    console.log(betObject);
+  }
+
+  homeTeamZero() {
+    this.homeTeam = false;
+    this.awayTeam = true;
+  }
+
+  awayTeamZero() {
+    this.awayTeam = false;
+    this.homeTeam = true;
   }
 
 }
