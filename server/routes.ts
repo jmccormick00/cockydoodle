3a5ec361bf852de56ee8c55ef80526c721f72642
jmccormick00 <jmccormick00@gmail.com>
2018-03-12 00:45:19 -0400

    comment out delete

--- server/routes.ts
+++ server/routes.ts
@@ -20,11 +20,11 @@ export default function setRoutes(app) {
 
   // Games
   router.route('/game').get(gameCtrl.getAll);
-  router.route('/game/count').get(gameCtrl.count);
+  //router.route('/game/count').get(gameCtrl.count);
   router.route('/game').post(gameCtrl.insert);
-  router.route('/game/:id').get(gameCtrl.get);
-  router.route('/game/:id').put(gameCtrl.update);
-  router.route('/game/:id').delete(gameCtrl.delete);
+  //router.route('/game/:id').get(gameCtrl.get);
+  //router.route('/game/:id').put(gameCtrl.update);
+  //router.route('/game/:id').delete(gameCtrl.delete);
   router.route('/game/close/:id').put(gameCtrl.close);
 
   // Users
@@ -34,7 +34,7 @@ export default function setRoutes(app) {
   //router.route('/user').post(userCtrl.insert);
   router.route('/user/:id').get(userCtrl.get);
   router.route('/user/:id').put(userCtrl.update);
-  router.route('/user/:id').delete(userCtrl.delete);
+  //router.route('/user/:id').delete(userCtrl.delete);
   router.route('/leaderboard').get(userCtrl.getLeaderboard);
 
   // Apply the routes to our application with the prefix /api
