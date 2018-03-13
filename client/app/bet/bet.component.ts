1def843615a58cd3298ac3257b22c0122689c93f
Will Hay Jr <willhayjr@gmail.com>
2018-03-13 18:58:48 -0400

    potential quick fix

--- client/app/bet/bet.component.ts
+++ client/app/bet/bet.component.ts
@@ -60,10 +60,9 @@ export class BetComponent implements OnInit {
         this.games = data;
         for(var i = 0; i < this.games.length; i++) {
           if (i < 4) {
-            if(this.games[i].homeTeam != "Radford") {
-              
+            if(this.games[i].homeTeam != "16 Radford") {
+              this.firstFour[i] = this.games[i];
             }
-            this.firstFour[i] = this.games[i];
           }
           if (i >= 4) {
             this.firstRound[i - 4] = this.games[i];
